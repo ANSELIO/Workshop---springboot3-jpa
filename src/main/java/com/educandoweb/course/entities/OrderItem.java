db687673f0c25ddd3e4c31cc570384aa8e11455b
ANSELIO <anseliojose@gmail.com>
2023-04-28 08:03:16 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -67,7 +67,11 @@ public class OrderItem implements Serializable {
 	public void setPrece(Double price) {
 		this.price = price;
 	}
-
+	
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
